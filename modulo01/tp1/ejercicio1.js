3b5a6b927569c5212edf13ba80e3668f63b155d7
Rocío Zárate <57687064+rocioz@users.noreply.github.com>
2022-10-07 09:17:44 -0300

    Create ejercicio1.js

---
+++ modulo01/tp1/ejercicio1.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ejercicio 1 </title>
+</head>
+<body>
+<script>
+//El código escribe mensajes en el navegador
+document.writeln("Hola Mundo!");
+document.writeln("Soy el primer script");
+</script>
+</body>
+</html>
