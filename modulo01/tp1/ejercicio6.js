975a935274e3f20e67398e113fb23a45db661c05
Rocío Zárate <57687064+rocioz@users.noreply.github.com>
2022-10-07 09:23:29 -0300

    Create ejercicio6.js

---
+++ modulo01/tp1/ejercicio6.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+var dias = ["domingo", 
+"lunes", 
+"martes", 
+"miercoles", 
+"jueves", 
+"viernes", 
+"sabado"];
+console.log("dias");
+</script>
+</body>
+</html>
