ae6bcfe3aaf1e6ac5c52397e817c02caf6ba2a52
Rocío Zárate <57687064+rocioz@users.noreply.github.com>
2022-10-07 09:24:39 -0300

    Create ejercicio7.js

---
+++ modulo01/tp1/ejercicio7.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+// define variables
+let dato, resultado;
+//lee datos y los concatena
+val1 = window.prompt("Introduce tu nombre", "...");
+val2 = window.prompt("Introduce tu apellido", "...");
+resultado = `Concatenado tu nombre y apellido es: ${val1} ${val2} ` ;
+//escribe el resultado
+document.write(resultado);
+</script>
+</body>
+</html>
