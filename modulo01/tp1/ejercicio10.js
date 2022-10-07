1798e50f85f8ecaa7beeaddd2200325128f1bc3b
Rocío Zárate <57687064+rocioz@users.noreply.github.com>
2022-10-07 09:26:33 -0300

    Create ejercicio10.js

---
+++ modulo01/tp1/ejercicio10.js
@@ -0,0 +1,15 @@
+var meses = [enero, 
+    febrero, 
+    marzo, 
+    abril, 
+    mayo, 
+    junio, 
+    julio, 
+    agosto, 
+    septiembre, 
+    octubre, 
+    noviembre, 
+    diciembre];
+
+
+document.write(meses[3])  
